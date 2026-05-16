5d06166180c0869e1ccaced4cda98342d96ee894
Herman S. <429230+has207@users.noreply.github.com>
2026-05-16 15:52:48 +0900

    [Vulkan] drop kEarlyHint in GetCurrentPixelShaderModification
    Fixes graphics device lost in Alan Wake (entering gameplay on new game)

--- src/xenia/gpu/vulkan/vulkan_pipeline_cache.cc
+++ src/xenia/gpu/vulkan/vulkan_pipeline_cache.cc
@@ -430,12 +430,17 @@ VulkanPipelineCache::GetCurrentPixelShaderModification(
       RenderTargetCache::Path::kHostRenderTargets) {
     using DepthStencilMode =
         SpirvShaderTranslator::Modification::DepthStencilMode;
-    if (shader.implicit_early_z_write_allowed() &&
-        (!shader.writes_color_target(0) ||
-         !draw_util::DoesCoverageDependOnAlpha(
-             regs.Get<reg::RB_COLORCONTROL>()))) {
-      modification.pixel.depth_stencil_mode = DepthStencilMode::kEarlyHint;
+    if (render_target_cache_.depth_float24_convert_in_pixel_shader() &&
+        normalized_depth_control.z_enable &&
+        regs.Get<reg::RB_DEPTH_INFO>().depth_format ==
+            xenos::DepthRenderTargetFormat::kD24FS8) {
+      modification.pixel.depth_stencil_mode =
+          render_target_cache_.depth_float24_round()
+              ? DepthStencilMode::kFloat24Rounding
+              : DepthStencilMode::kFloat24Truncating;
     } else {
+      // kEarlyHint was tried here but it seems to trigger GPU fault on nvidia
+      // (entering gameplay in Alan Wake), so going with the safe alternative.
       modification.pixel.depth_stencil_mode = DepthStencilMode::kNoModifiers;
     }
 
